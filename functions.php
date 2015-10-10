1188c507556f4287cfd291d81e3afde19c091e72
Eiko <gaia.e.h@gmail.com>
2015-10-11 00:10:14 +0800

    修正 Disable Emoji 函数冲突
    毕竟紫菜鸡开着插件

--- functions.php
+++ functions.php
@@ -63,7 +63,8 @@ add_action( 'wp_enqueue_scripts', 'eiko_scripts' );
 /**
 * Disable the emoji's
 */
-function disable_emojis() {
+if (!function_exists('disable_emojis')) {
+  function disable_emojis() {
     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
     remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
     remove_action( 'wp_print_styles', 'print_emoji_styles' );
@@ -72,11 +73,13 @@ function disable_emojis() {
     remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
     remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
     add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+  }
+  function disable_emojis_tinymce( $plugins ) {
+    return array_diff( $plugins, array( 'wpemoji' ) );
+  }
+  add_action( 'init', 'disable_emojis' );
 }
-function disable_emojis_tinymce( $plugins ) {
-  return array_diff( $plugins, array( 'wpemoji' ) );
-}
-add_action( 'init', 'disable_emojis' );
+
 
 /*
   Gravatar 头像地址替换
